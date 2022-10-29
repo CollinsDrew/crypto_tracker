853aa93c729f4fb7c813fb2857129e54be5402fc
CollinsDrew <collinsdrew1990@gmail.com>
2022-10-29 10:24:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+import Navbar from "./Components/navbar";
+import CoinSearch from "./Components/coinSearch";
+import Footer from "./Components/footer";
+
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
