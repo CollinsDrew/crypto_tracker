80f6681fc77a58d30ecb899b9398a3d3f67f0d57
CollinsDrew <collinsdrew1990@gmail.com>
2022-10-29 11:19:16 +0100

    declared useState variables and api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import React from "react";
-import Navbar from "./Components/navbar";
-import CoinSearch from "./Components/coinSearch";
-import Footer from "./Components/footer";
+
 
 
 function App() {
   return (
     
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+
   );
 }
 
