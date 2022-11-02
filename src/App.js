e20a218449aac88fd8aacb71c6cf4b8cfd6dc759
CollinsDrew <collinsdrew1990@gmail.com>
2022-11-02 20:56:11 +0000

    CC logo added

--- src/App.js
+++ src/App.js
@@ -1,59 +1,66 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
-import './App.css';
-import Coins from './Coins';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Coins from "./Coins";
 
 function App() {
-const[coins,setCoins]=useState([]);
-const[search,setSearch]=useState('');
-// const timer = setTimeout(() => {
-//   console.log('This will run after 1 second!')
-// }, 300000);
- 
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  // const timer = setTimeout(() => {
+  //   console.log('This will run after 1 second!')
+  // }, 300000);
 
+  // API and render as soon as page is loaded
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-// API and render as soon as page is loaded
-useEffect(()=>{
-axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
-.then(res=>{
-setCoins(res.data);
-console.log(res.data);
-}).catch(error=>console.log(error));
-},[]);
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
 
-const handleChange=e=>{
-setSearch(e.target.value);
-}
-
-const filteredCoins=coins.filter(coin=>
-coin.name.toLowerCase().includes(search.toLowerCase())
-)
-
-return (
+  return (
     <div className="coin-app">
-        <div className="coin-search">
-        <h1 className="coin-text">Crypto Collins</h1>
+      <div className="coin-search">
+        <img src={require("./CC.png")} alt="logo"></img>
+        {/* <h1 className="coin-text">Crypto Collins</h1> */}
         <form>
-        <input type="text"
+          <input
+            type="text"
             onChange={handleChange}
             placeholder="Search"
-            className="coin-input" />
+            className="coin-input"
+          />
         </form>
-        </div>
-        {filteredCoins.map(coin=>{
-        return(
-        <Coins key={coin.id}
-        name        =   {coin.name}
-        image       =   {coin.image}
-        symbol      =   {coin.symbol}
-        marketcap   =   {coin.market_cap}
-        price       =   {coin.current_price}
-        priceChange =   {coin.price_change_percentage_24h}
-        volume      =   {coin.total_volume}
-        />
-        )})}
+      </div>
+      {filteredCoins.map((coin, index) => {
+        return (
+          <Coins
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+            index={index}
+          />
+        );
+      })}
     </div>
-);
+  );
 }
 export default App;
--- src/Coins.js
+++ src/Coins.js
@@ -1,57 +1,55 @@
-import React from 'react'
-import './Coins.css'
-export const Coins = ({name,image,symbol,price,volume, priceChange,marketcap}) => {
+import React from "react";
+import "./Coins.css";
+export const Coins = ({
+  name,
+  image,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketcap,
+  index,
+}) => {
+  const showDisplay = true;
+  const _index = 0;
 
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        {/* Coin div */}
+        <div className="coin">
+          <img src={image} alt="crypto" />
 
-    const showDisplay = true;
+          <h1>{name}</h1>
 
-return (
-        <div className="coin-container">
-            <div className="coin-row">
-                {/* Coin div */}
-                <div className="coin">
-                    <img src={image} alt="crypto"/>
-
-                    <h1>{name}</h1>
-
-                    <p className="coin-symbol">{symbol}</p>
-
-                </div>
-                {/* Coin data div */}
-                <div className="coin-data">
-                    <p className="coin-price">${price}</p>
-
-                    <p className="coin-volume">${volume.toLocaleString()}</p>
-                    
-                    {
-                    priceChange < 0 ?(
-                    <p className="coin-percent red">
-                    {priceChange.toFixed(2)}%
-                    </p>
-                    ):(
-                    <p className="coin-percent green">
-                    {priceChange.toFixed(2)}%
-                    </p>
-                    )
-                    }
-
-                    <p className="coin-marketing">
-                    Mkt Cap: ${marketcap.toLocaleString()}
-                    </p>
-                </div>
-                {/* Extra Coin data for charts div */}
-                {showDisplay ?(
-
-                    <div className='row'>
-
-                    <p>Charts</p>
-
-                    </div>
-
-                ): ""}
-                
-            </div>
+          <p className="coin-symbol">{symbol}</p>
         </div>
-    )
-}
+        {/* Coin data div */}
+        <div className="coin-data">
+          <p className="coin-price">${price}</p>
+
+          <p className="coin-volume">${volume.toLocaleString()}</p>
+
+          {priceChange < 0 ? (
+            <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+          )}
+
+          <p className="coin-marketing">
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
+        </div>
+        {/* Extra Coin data for charts div */}
+        {showDisplay && index === _index ? (
+          <div className="row">
+            <p>Charts</p>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
+};
 export default Coins;
